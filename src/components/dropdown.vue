784c1f535ccf465289d041c9e689778f11e6cb12
chenjie.ma <295526320@qq.com>
2018-12-26 21:20:04 +0800

    add pages

--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -8,7 +8,7 @@
             <a class="dropdown-item">Copy</a>
             <a class="dropdown-item">Find</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item1">Select All</a>
+            <a class="dropdown-item">Select All</a>
         </div>
     </div>
 </template>
